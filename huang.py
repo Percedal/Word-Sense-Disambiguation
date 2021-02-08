869e6d02344de3355b3a5123050e1fbfeec88d65
Percedal <moi.durand.72@gmail.com>
2021-02-08 17:51:22 +0100

    docstring

--- huang.py
+++ huang.py
@@ -5,7 +5,22 @@ from gensim.models import Word2Vec
 from nltk.cluster import KMeansClusterer, cosine_distance
 
 class Huang:
-    """TODO"""
+    """Implementation of the huang et al. algorithme for Word Sens Disambiguation (WSD).
+    
+    Attributs
+    ---------
+    ctx_window: int
+        Contextual window size. A context define the words that appears before and after a polysemous.
+        This context is used to disambiguate words with mutiple senses.
+        When a context is compute, it is center on a word and this parameter define the total length of all contexts.
+        Meaning for a context size of 11, 5 words will taken before and 5 will taken after the center word. Those 11 words compose the context.
+    seed: int
+        Random generator seed.
+
+    See Also
+    --------
+    'Improving Word Representations via Global Context and Multiple Word Prototypes' huang et al., 2012
+    """
 
     def __init__(self, vector_size=100, ctx_window=11, seed=random.randrange(1e9)):
         """
@@ -33,7 +48,7 @@ class Huang:
         """
         self.huang(sentences, senses_dict)
     
-    def extractWindow(self, sentence, pos):
+    def __extractWindow(self, sentence, pos):
         """Extract a sub-sequence (window) from a list.
         Size of the extract window is define by ``self.ctx_window``.
         
@@ -98,7 +113,7 @@ class Huang:
 
         res = []
         for idx in self.__getListIndices(sentence, word):
-            ctx_vec = self.__computeContextVector(self._wv, self.extractWindow(sentence, idx))
+            ctx_vec = self.__computeContextVector(self._wv, self.__extractWindow(sentence, idx))
             _,ms = self.__mostSimilare(ctx_vec, self._senses[word])
             res.append(ms)
 
@@ -141,7 +156,7 @@ class Huang:
                     mean_vectors.append(
                         self.__computeContextVector(
                             w2v.wv,
-                            self.extractWindow(sentence, idx)
+                            self.__extractWindow(sentence, idx)
                         )
                     )
              
@@ -205,20 +220,60 @@ class Huang:
     
     @staticmethod
     def __computeContextVector(wv, window):
-        """TODO"""
-        #TODO : changer la somme en moyenne (pour normaliser les vecteurs)
-        return np.sum([wv[word] for word in window], axis=0)
+        """Compute a contextual vector
+        
+        Parameters
+        ----------
+        wv: dict
+            A word vector dictionary that map word with their vector representations
+        window: list
+            List of string
+        
+        Returns
+        -------
+        list
+            Return a word vector that is the average of the words in the given sentence.
+        """
+        return np.mean([wv[word] for word in window], axis=0)
     
     @staticmethod
     def __getListIndices(lst, item):
-        """TODO"""
+        """Get the indices in the list (``lst``) that are equal to the given ``item``
+        
+        Parameters
+        ----------
+        lst: list
+            List of items
+        item: any
+            The item to find in the list
+
+        Yields
+        ------
+        int
+            Index of a matching ``item`` in the list
+        """
         for i in range(len(lst)):
             if lst[i] == item:
                 yield i
     
     @staticmethod
     def __mostSimilare(vector, candidates):
-        """TODO"""
+        """Return from a list of vectors (``candidates``) the most cosine similare to the given ``vector``
+        
+        Parameters
+        ----------
+        vector: list
+            A vector
+        candidates: list of list
+            A list of vectors
+        
+        Returns
+        -------
+        int
+            Index of the most similare vector in the candidates list
+        list
+            A vector that is the most similare to the given one
+        """
         idx = 0
         for i,c in enumerate(candidates):
             if cosine_distance(vector, c) > cosine_distance(vector, candidates[idx]):
